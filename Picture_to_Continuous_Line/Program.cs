a6394b526046d01c2a0d22e67c40fb3f618e0346
Christian <christian220h@outlook.com>
2020-06-29 22:35:50 -0400

    Update Program.cs

--- Picture_to_Continuous_Line/Program.cs
+++ Picture_to_Continuous_Line/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Windows.Forms;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace Picture_to_Continuous_Line
 {
@@ -16,7 +13,6 @@ namespace Picture_to_Continuous_Line
         public static int square = 11;
         public static int scaleMod = 1;
         public static int scaleMultiplier;
-        public static int lineSkip;
         public static int pixelCount = 0;
         public static int blocksProc = 0;
         public static int blocks = 0;
@@ -25,161 +21,127 @@ namespace Picture_to_Continuous_Line
         {
             OpenFileDialog fd = new OpenFileDialog();
             fd.ShowDialog();
-            string filePath = fd.FileName;
-            Image startImg = Image.FromFile(filePath);
+            string filePath = fd.FileName;            
+            
             Console.WriteLine("File Read Successfully");
-            Console.WriteLine($"Dimensions {startImg.Width} x {startImg.Height}");
+            Console.WriteLine($"Dimensions {Image.FromFile(filePath).Width} x {Image.FromFile(filePath).Height}");
             Console.Write("Scale: ");
             scaleMultiplier = Convert.ToInt32(Console.ReadLine()) * scaleMod;
-            Console.Write("Lines per block row: ");
-            lineSkip = Convert.ToInt32(Console.ReadLine());
-            Image img = ResizeImage(startImg, GetNearestWholeMultiple(startImg.Width, square) * scaleMultiplier, GetNearestWholeMultiple(startImg.Height, square) * scaleMultiplier);
-            Bitmap bitmapProcessing = new Bitmap(img);
-            Bitmap bitmapResult = new Bitmap(img.Width, img.Height, img.PixelFormat);
+            Bitmap bitmapProcessing = new Bitmap(ResizeImage(Image.FromFile(filePath), GetNearestWholeMultiple(Image.FromFile(filePath).Width, square) * scaleMultiplier, GetNearestWholeMultiple(Image.FromFile(filePath).Height, square) * scaleMultiplier));
+            
+            Bitmap bitmapResult = new Bitmap(bitmapProcessing.Width, bitmapProcessing.Height, bitmapProcessing.PixelFormat);
 
             Graphics graphics = Graphics.FromImage(bitmapResult);
 
             SolidBrush brush = new SolidBrush(Color.White);
-            Rectangle rect = new Rectangle(new Point(0, 0), new Size(img.Width, img.Height));
+            Rectangle rect = new Rectangle(new Point(0, 0), new Size(bitmapProcessing.Width, bitmapProcessing.Height));
             graphics.FillRectangle(brush, rect);
 
             //bitmapResult.MakeTransparent();
 
             int height = bitmapProcessing.Height;
             int width = bitmapProcessing.Width;
-            int[,] avgArr = new int[bitmapProcessing.Width, bitmapProcessing.Height];
+
             Console.Title = "Spline Art";
             Console.WriteLine($"Pixel Matrix size: {width} x {height}");
             int area = width * height;
             blocks = area / (square * square);
 
-            for (int y = 0; y < height; y++) // Generates the 
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    //Color color = bitmapProcessing.GetPixel(x, y);
-                    //Luminocity
-                    //avgArr[x, y] = (int)((0.21 * color.R) + (0.72 * color.G) + (0.07 * color.B));
-                    //Average
-                    avgArr[x, y] = (int)(bitmapProcessing.GetPixel(x, y).GetBrightness() * 255);
-                    //Lightness
-                    //avgArr[x, y] = (int)(Math.Max(color.R,Math.Max(color.G,color.B)) + 
-                    //                     Math.Min(color.R, Math.Min(color.G, color.B)))/2;
-                }
-                Console.Write($"\rComplete: %{Math.Round((double)((double)y / (double)bitmapProcessing.Height*100), 0)}");
-            }
+            Point[] pointMap;
+            Pen whitePen = new Pen(Color.Black, 1);
+
+            int widthProg = width / square;
+            int heightProg = height / square;
 
             Console.WriteLine();
-            var pointMap = generatePointMap(avgArr);
-            var whitePen = new Pen(Color.Black, 1);
-            for (int j = 0; j < pointMap.Count;)
+            for (int y = 0; y < heightProg; y++)
             {
-                Point[] p = pointMap.ElementAt(j);
-                Point[] points = new Point[2];
-                for (int i = 0; i < p.Length - 3;)
+                for (int x = 0; x < widthProg; x++)
                 {
-                    if ((p.ElementAt(i).X + 1 == p.ElementAt(i + 1).X) && (p.ElementAt(i).X != 0 & p.ElementAt(i).Y != 0))
+                    int avg = 0;
+                    for(int i = 0; i < square; i++)
                     {
-                        points[0] = p.ElementAt(i);
-                        points[1] = p.ElementAt(i + 1);
+                        for(int j = 0; j < square; j++)
+                        {
+                            double pixel = (int)(bitmapProcessing.GetPixel(j + (square * x), i + (square * y)).GetBrightness() * 255);
+                            avg += (int)Math.Round(pixel);
+                        }
                     }
-                    else if ((p.ElementAt(i).X == 0) && (p.ElementAt(i).Y == 0))
+
+                    avg /= (square * square);
+
+                    bool bitches = true;
+                    pointMap = generatePointMap(avg, x, y);
+                    
+                    foreach(Point bitch in pointMap)
                     {
-                        points[0] = p.ElementAt(i);
-                        points[1] = p.ElementAt(i);
+                        if(bitch == new Point(0,0))
+                        {
+                            bitches = false;
+                            break;
+                        }
                     }
-                    graphics.DrawCurve(whitePen, points, 0.5f);
-                    i += 1;
+
+                    if(bitches)
+                        graphics.DrawCurve(whitePen, pointMap, 1.0f);
+                    Console.Write("\rComplete: %{0}", Math.Round((((double)((y*widthProg) + x) / (widthProg * heightProg)) * 100.0)));
                 }
-                j += lineSkip;
-                Console.Write($"\rPopulating line segment {j}/{pointMap.Count}  %{Math.Round((double)((double)j * 100) / (double)pointMap.Count, 0)}");
             }
-            ResizeImage(bitmapResult, img.Width, img.Height);
+            ResizeImage(bitmapResult, Image.FromFile(filePath).Width, Image.FromFile(filePath).Height);
             bitmapResult.Save(@"C:\Users\chris\Desktop\test4.png");
             Console.WriteLine("\n\nProcessing Complete");
             Console.Read();
         }
 
-        static List<Point[]> generatePointMap(int[,] inputArr)
+        static Point[] generatePointMap(int avg, int x, int y)
         {
-            int blocksWide = (int)Math.Floor((double)inputArr.GetLength(0) / square);
-            int blocksTall = (int)Math.Floor((double)inputArr.GetLength(1) / square);
+            List<Point> resultList = new List<Point>();
 
-            List<Point[]> resultList = new List<Point[]>();
+            x *= square;
+            y *= square;
+            int pixelAvg = avg;
 
-            for (int height = 0; height < blocksTall; height++) //Cycles through inputArr by height in blocks
-            {
-                int extraPixels = 0;
-                Point[] resultArr = new Point[0];
-                var rslt = new List<Point>();
-                for (int width = 0; width < blocksWide; width++) //Cycles through inputArr by width in blocks
-                {
-                    int[,] tempArr = new int[square, square];
-                    for (int y = 0; y < square; y++)
-                    {
-                        for (int x = 0; x < square; x++)
-                        {
-                            tempArr[x, y] = inputArr[width * square + x, height * square + y];
-                        }
-                    }
+            resultList.AddRange(generateSplineData(pixelAvg, square, x, y));
 
-                    int pixelAvg = 0;
-                    foreach (int i in tempArr) //Cycles through pixels in {square} by {square} grid
-                    {
-                        pixelAvg += i;
-                    }
-                    pixelAvg = pixelAvg / (square * square);
+            //Console.Write($"\rProcessing Blocks: %{Math.Round((double)((double)height / (double)blocksTall * 100), 0)}");
 
-                    rslt.AddRange(generateSplineData(pixelAvg, square, width, height));
-
-                    for (int x = 0; x < rslt.Count; x++)
-                    {
-                        if (rslt.Count != 0)
-                        {
-                            resultArr.Append(rslt.ElementAt(x));
-                            //resultArr[(width * square) + x + 1] = rslt[x];
-                        }
-                    }
-                    extraPixels++;
-                }
-
-                Console.Write($"\rProcessing Blocks: %{Math.Round((double)((double)height / (double)blocksTall * 100), 0)}");
-                
-                if (rslt.ToArray().Length > 0)
-                    resultList.Add(rslt.ToArray());
-            }
-            Console.WriteLine();
-            return resultList;
+            Point[] points = resultList.ToArray();
+            return points;
         }
 
-        public static List<Point> generateSplineData(int average, int square, int blkWidth, int blkHeight)
+
+
+
+        public static List<Point> generateSplineData(int average, int square, int x, int y)
         {
-            int[] heightMap = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0 };
+            int[] heightMap = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,  0,  0 };
+            //int[] heightMap = { 6, 5, 4, 3, 2, 1, 0};
+            //int[] heightMap = { 9, 7, 4, 1, 0, 0 };
+            //int[] heightMap = {  0,  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
             //int[] heightMap = { 6, 5, 4, 3, 2, 2, 1, 1, 1, 0 };
             //int[] heightMap = { 0, 0, 1, 1, 2, 4, 5 }; //Inverted
 
             int heightFreq = (((average - 0) * (heightMap.Length - 1 - 0)) / (255 - 0)) + 0;
 
-            int height = heightMap[heightFreq];
+            int height = heightMap[heightFreq];// * scaleMultiplier;
             //int height = heightMap[heightMap.Length - heightFreq - 1]; //Inverted
 
             List<Point> pointMap = new List<Point>();
             //if (!(average > 255 -50) && !(heightMap[heightFreq] == 0))
-            if (!(heightMap[heightFreq] == 0)) //Inverted
+            if ((heightMap[heightFreq] == 0)) //Inverted
+            {
+                pointMap.Add(Point.Empty);
+                return pointMap;
+            }
+            else
             {
                 //pointMap.Add(new Point((blkWidth * square), (blkHeight * square)));
                 for (int i = 0; i < square; i++)
                 {
-                    if (i % 2 == 0 || i == 0)
-                    {
-                        pointMap.Add(new Point(i + blkWidth * square, height + blkHeight * square));
-                        pointMap.Add(new Point(i + blkWidth * square, +blkHeight * square));
-                    }
-                    else
-                    {
-                        pointMap.Add(new Point(i + blkWidth * square, height + blkHeight * square));
-                        pointMap.Add(new Point(i + blkWidth * square, blkHeight * square));
-                    }
+                    //pointMap.Add(new Point(i + x, y - height));
+                    //pointMap.Add(new Point(i + x, height + y));
+                    pointMap.Add(new Point(i + x, height + y));
+                    pointMap.Add(new Point(i + x, y));
                 }
             }
             return pointMap;
@@ -187,12 +149,12 @@ namespace Picture_to_Continuous_Line
 
         public static Bitmap ResizeImage(Image image, int width, int height)
         {
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
+            Rectangle destRect = new Rectangle(0, 0, width, height);
+            Bitmap destImage = new Bitmap(width, height);
 
             destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
 
-            using (var graphics = Graphics.FromImage(destImage))
+            using (Graphics graphics = Graphics.FromImage(destImage))
             {
                 graphics.CompositingMode = CompositingMode.SourceCopy;
                 graphics.CompositingQuality = CompositingQuality.HighQuality;
@@ -200,7 +162,7 @@ namespace Picture_to_Continuous_Line
                 graphics.SmoothingMode = SmoothingMode.HighQuality;
                 graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
-                using (var wrapMode = new ImageAttributes())
+                using (ImageAttributes wrapMode = new ImageAttributes())
                 {
                     wrapMode.SetWrapMode(WrapMode.TileFlipXY);
                     graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
@@ -211,12 +173,15 @@ namespace Picture_to_Continuous_Line
 
         public static int GetNearestWholeMultiple(double input, double X)
         {
-            var output = Math.Round(input / X);
-            if (output == 0 && input > 0) output += 1;
+            double output = Math.Round(input / X);
+            if (output == 0 && input > 0)
+            {
+                output += 1;
+            }
+
             output *= X;
 
             return (int)output;
         }
     }
 }
-    
